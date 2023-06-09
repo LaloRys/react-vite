4902760d2ad0afc015fa0651f7d933fde18e7d68
LaloRys <38341031+LaloRys@users.noreply.github.com>
2023-06-09 14:36:19 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import {Login} from "./pages/Login";
 import { Home } from "./pages/Home";
@@ -27,7 +27,6 @@ function App() {
   return (
     <div className="App">
       <TaskContextProvider>
-<<<<<<< HEAD
         <Navbar/>
 
         <div className="container">
@@ -38,18 +37,6 @@ function App() {
           <Route path="/*" element={<NotFound />} />
         </Routes>
         </div>
-=======
-        <Navbar/>  
-          <BrowserRouter
-          basename={'/react-vite'}
-          >
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/Login" element={<Login />} />
-              <Route path="/*" element={<NotFound />} />
-            </Routes>
-          </BrowserRouter>
->>>>>>> b9bb96bb3d6333c540619cc12bead90d7409ad7a
       </TaskContextProvider>
     </div>
   );
